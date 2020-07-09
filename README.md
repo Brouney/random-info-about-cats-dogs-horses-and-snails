4adece9a4ead7509a6a228ed12a9bc1330db56c5
Brouney <piotreg98@gmail.com>
2020-07-09 11:42:48 +0200

    added some features

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # random info about cats
-random info about cats - exercise with json and requests in java
+random info about cats, horses, dogs and snails - exercise with json and requests in java<br>
+Free API - https://cat-fact.herokuapp.com
--- restclient/src/main/java/pl/brunarski/restclient/RestClient.java
+++ restclient/src/main/java/pl/brunarski/restclient/RestClient.java
@@ -12,12 +12,39 @@ public class RestClient {
 	
 	public static void main(String []args) throws IOException {
 		
-		URL url = new URL("https://cat-fact.herokuapp.com/facts/random");
+		URL urlcat = new URL("https://cat-fact.herokuapp.com/facts/random");
 		
-		InputStreamReader reader = new InputStreamReader(url.openStream());
+		InputStreamReader readercat = new InputStreamReader(urlcat.openStream());
 		
-		CatModel catmodel = new Gson().fromJson(reader,CatModel.class);
-		System.out.print(catmodel.getText());
+		CatModel catmodel = new Gson().fromJson(readercat,CatModel.class);
+		System.out.println("Random info about Cats:");
+		System.out.println("\t"+catmodel.getText()+"\n");
+		
+		
+		URL urldog = new URL("https://cat-fact.herokuapp.com/facts/random?animal_type=dog");
+		
+		InputStreamReader readerdog = new InputStreamReader(urldog.openStream());
+		
+		DogModel dogmodel = new Gson().fromJson(readerdog,DogModel.class);
+		System.out.println("Random info about Dogs:");
+		System.out.println("\t"+dogmodel.getText()+"\n");
+		
+		URL urlsnail = new URL("https://cat-fact.herokuapp.com/facts/random?animal_type=snail");
+		
+		InputStreamReader readersnail = new InputStreamReader(urlsnail.openStream());
+		
+		DogModel snailmodel = new Gson().fromJson(readersnail,DogModel.class);
+		System.out.println("Random info about Snails:");
+		System.out.println("\t"+snailmodel.getText()+"\n");
+		
+		
+		URL urlhorse = new URL("https://cat-fact.herokuapp.com/facts/random?animal_type=horse");
+		
+		InputStreamReader readerhorse= new InputStreamReader(urlhorse.openStream());
+		
+		DogModel horsemodel = new Gson().fromJson(readerhorse,DogModel.class);
+		System.out.println("Random info about Snails:");
+		System.out.println("\t"+horsemodel.getText());
 		
 	}
 
